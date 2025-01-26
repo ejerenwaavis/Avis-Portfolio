8d4b3f8c93e09e0f42f68fe7f1e465573b318313
Avis Ejerenwa <ejerenwaavis.com>
2025-01-26 17:47:48 -0500

    added logger

--- src/app.js
+++ src/app.js
@@ -4,12 +4,16 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const publuicPath = path.join(__dirname, 'public');
+const publicPath = path.join(__dirname, 'public');
+
+
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public'))); 
 app.use(express.static(path.join(__dirname, 'public/avis')));
 
 // View engine setup
@@ -19,12 +23,12 @@ app.set('view engine', 'ejs');
 // Routes
 // Home route
 app.get('/', (req, res) => {
-    res.render('home', { title: 'Home' });
+    res.render('home', { title: 'Home', domain:publicPath });
 });
 
 // About route
 app.get('/404', (req, res) => {
-    res.render('partials/404', { title: 'About' });
+    res.render('partials/404', { title: 'Page Not Found',  domain:publicPath });
 });
 
 // Resume route to download PDF
@@ -37,27 +41,18 @@ app.get('/resume', (req, res) => {
     });
 });
 
-// 404 Page (Optional)
+// Handle 404 errors
 app.use((req, res) => {
-    res.status(404).render('partials/404', { title: '404 - Page Not Found' });
+    res.status(404).render('partials/404', { title: '404 - Page Not Found', domain:publicPath });
 });
 
 // Start the server
 app.listen(PORT, () => {
     outputLog(`Server is running on http://localhost:${PORT}`);
-    outputLog(publuicPath);
-
+    outputLog(path.join(__dirname, 'public'));
 });
 
-
-
-
-
-
-
-
-// helper functions
-
+// Helper functions
 function outputLog(message) {
     console.log(new Date().toLocaleString({ dateStyle: 'short', timeStyle: 'short' }), '--->', message);
 }
